7298462fe47a6582375b347361258353918220e0
unknown <prinjalboruah@gmail.com>
2021-10-11 03:55:55 +0530

    removed extra space from dictionary value

--- sacumen.py
+++ sacumen.py
@@ -39,9 +39,9 @@ class LogsDict():
         len_matched_list = len(matched_positions)
         for i in range(len_matched_list):
             if i != (len_matched_list-1):
-                final_dict[self.input_str[ matched_positions[i][0] : (matched_positions[i][1] - 1) ]] = self.input_str[ matched_positions[i][1] : (matched_positions[i+1][0]) ]
+                final_dict[self.input_str[ matched_positions[i][0] : (matched_positions[i][1] - 1) ]] = (self.input_str[ matched_positions[i][1] : (matched_positions[i+1][0]) ]).strip()
             else:
-                final_dict[self.input_str[ matched_positions[i][0] : (matched_positions[i][1] - 1) ]] = self.input_str[ matched_positions[i][1] : ]
+                final_dict[self.input_str[ matched_positions[i][0] : (matched_positions[i][1] - 1) ]] = (self.input_str[ matched_positions[i][1] : ]).strip()
         return final_dict
 
     def get_response(self):
@@ -56,6 +56,8 @@ class LogsDict():
         return final_dict
 
 if __name__ == "__main__":
+    #test below
+    
     inp_str = "SAC:0|Sacumen|CAAS|2021.2.0|3|MALICIOUS|High|cat=C2 cs1Label=subcat cs1=DNS_TUNNELING cs2Label=vueUrls cs2=https://aws-dev.sacdev.io/alerts?filter=alertId%3D%3D81650 cs3Label=Tags cs3=USA,Finance cs4Label=Url cs4=https://aws-dev.sacdev.io/settings/tir?rules.sort=4%3A1&filter=state%3D%3D2&selected=9739323 cn1Label=severityScore cn1=900 msg=Malicious activity was reported in CAAS\= A threat intelligence rule has been automatically created in DAAS. dhost=bad.com dst=1.1.1.1"
     logs_dict_instance = LogsDict(inp_str)
     print(logs_dict_instance.get_response())
